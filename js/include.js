ade02ac09cf11177239921cc8eac872a96f11666
pandaintdesign <pandaintdesign@gmail.com>
2025-05-12 15:01:55 +0900

    build: footer, header html 분리. include.js로 공통영역지정 / style: command.css, home.css 수정, header.css 분리추가, footer.css 생성

---
+++ js/include.js
@@ -0,0 +1,11 @@
+//header, footer 공통영역으로 분리
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("./header.html") // 루트 기준이므로 슬래시(/)로 시작
+    .then((res) => res.text())
+    .then((data) => (document.getElementById("header").innerHTML = data));
+
+  fetch("./footer.html")
+    .then((res) => res.text())
+    .then((data) => (document.getElementById("footer").innerHTML = data));
+});
